c741c625c3630d7cbccb6541205dd4d37f4fb87f
kjy042552-lgtm <kjy042552@gmail.com>
2026-04-01 02:34:37 +0000

    5주차 수업 (+과제)

--- chapter08/lab1inputarray.c
+++ chapter08/lab1inputarray.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
+	int input[20] = {0};
+	int i = 0;
 	//초기화로 모든 원소에 0을 저장
 	// 크기가 20인 int형 배열 input을 선언하고 모든 원소를 0으로 초기화하세요
 
@@ -10,6 +12,15 @@ int main(void)
 	printf("0을 입력하면 입력을 종료합니다.\n");
 	// 인덱스 변수 i를 0으로 초기화하세요
 	// do-while문을 사용해 scanf("%d", &input[i]); 로 정수를 입력받고, input[i++] != 0인 동안 반복하세요
+	do{
+		scanf("%d", &input[i]);
+	}while (input[i++] != 0);
+		i = 0;
+	while (input[i] !=0)
+	{
+		printf("%d", input[i]);
+		i++;
+	}
 
 	// i를 0으로 초기화하세요
 	// input[i]가 0이 아닌 동안 printf("%d ", input[i++]); 형식으로 출력하는 while문을 작성하세요
